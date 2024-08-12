7983a1f6082141436cb083b76f22f8ffb1fbf33d
Keshav Garg <keshav.garg.5130@gmail.com>
2024-08-12 14:26:06 +0530

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
 import Header from './Header'
 
 const Login = () => {
+    const [isSignInForm, setIsSignInForm] = useState(true)
+
+const toggleSignInForm = () => {
+  setIsSignInForm(!isSignInForm)
+}
   return (
     <div>
         <Header/>
@@ -10,11 +16,12 @@ const Login = () => {
             alt='bg logo' />
         </div>
         <form className='w-3/12 absolute p-12 my-36 mx-auto right-0 left-0 text-white bg-black rounded bg-opacity-70'>
-            <h1 className='font-bold text-3xl py-6 my-4 mx-auto'>Sign In</h1>
+            <h1 className='font-bold text-3xl py-6 my-4 mx-auto'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+            {!isSignInForm && <input type='text' placeholder=' Full Name' className='text-sm p-4 my-4 mx-auto w-full rounded-sm bg-black bg-opacity-40 border border-current' />}
             <input type='text' placeholder=' Email' className='text-sm p-4 my-4 mx-auto w-full rounded-sm bg-black bg-opacity-40 border border-current' />
             <input type='password' placeholder='Password' className='text-sm p-4 my-4 mx-auto w-full rounded-sm  bg-black bg-opacity-40 border border-current' /> 
-            <button className='p-2 my-8 mx-auto bg-red-600 w-full rounded-sm'>Sign In</button>
-            <p className='text-sm'>New to Netflix? <a>Sign up now</a></p>
+            <button className='p-2 my-8 mx-auto bg-red-600 w-full rounded-sm'>{isSignInForm ? "Sign In" : "Sign Up"}</button>
+            <p className='text-sm cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign up now!" : "Already registered? Sign in now!"}</p>
         </form>
     </div>
   )
